6e39005de0a4981ba2bcb645d7e58ccbc5b7d512
Felipe Assis <felipe.assis.it@gmail.com>
2020-07-22 22:06:43 -0300

    Integracao do botao de upvote com o backend

---
+++ Brewing Process Unity/Assets/Scripts/Request/Comment/UpvoteComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Object = System.Object;
+
+namespace Request.Comment
+{
+    public class UpvoteComment : MonoBehaviour
+    {
+        private HttpRequest _httpRequest;
+
+        void Awake()
+        {
+            _httpRequest = FindObjectOfType<HttpRequest>();            
+        }
+
+        public void Upvote(Guid trackId, Guid stageId, Guid consultantId, long dialogIndex)
+        {
+            StartCoroutine(_httpRequest.PostRequest($"{trackId}/{stageId}/{dialogIndex}/{consultantId}", null));
+        }
+    }
+}
