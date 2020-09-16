7670a6d3bb97330f093b723d1afdb81104032ceb
marvintomizawa <marvintomizawa@hotmail.com>
2020-09-16 20:48:20 -0300

    feature: Adicionado componente de transição de tela

--- Brewing Process Unity/Assets/Scripts/UI/SceneTransition.cs
+++ Brewing Process Unity/Assets/Scripts/UI/SceneTransition.cs
@@ -1,40 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
-    
-    // Use this for initialization
-    void Start()
-    {
-    }
 
-    void Update()
+#pragma warning disable 0649
+    [SerializeField] private Animator transition;
+    [SerializeField] private Slider progressSlider;
+#pragma warning restore 0649
+
+    public void GoToMainMenu()
     {
-    }
-    
-    public void GoToMainMenu() 
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        LoadSceneWithLoading("MainMenu");
     }
 
     public void GoToTrackScene()
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("TrackScene");
+        LoadSceneWithLoading("TrackScene");
     }
     
     public void GoToDialogueScene()
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Dialogue");
+        LoadSceneWithLoading("Dialogue");
     }
     
     public void GoToFirstGame()
     {
+        LoadSceneWithLoading("Moer");
+    }
+
+    private void LoadSceneWithLoading(string sceneName)
+    {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Moer");
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        var operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            progressSlider.value = progress;
+            yield return null;
+        }
     }
 }
     
